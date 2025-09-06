b14cc069c3948b3deca5a66c4ee2017f60814790
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-09-06 13:59:05 +0100

    changes in design

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -4,8 +4,12 @@ import React from 'react';
 import styles from './HeroSection.module.css';
 import Button from './Button';
 import mainPic from '../assets/main-pic.svg';
-
+import profilePic from '../assets/hero.png';
+import profilePic2 from '../assets/hugohero2.png';
 import comp1 from "../assets/hero/Component-1.svg"    
+import decBottom from "../assets/dec-bottom.png"
+import decTop from "../assets/dec-top.png"
+import decTopOne from "../assets/dec-top-1.png"
 
 
 const HeroSection: React.FC = () => {
@@ -25,11 +29,14 @@ const HeroSection: React.FC = () => {
         a developer and Science Computing student based in Dublin. This is my portfolio, showcasing my projects, my story, and how we can work together.
         </p>
           <Button btnType='link' href='#aboutMe'>MORE INFO</Button>
+          <img className={styles.heroDecTopOne} src={decTopOne} alt="Decoration Top" />
         </div>
 
         <div className={styles.heroImageWrapper}>
-          <img className={styles.heroImage} src={mainPic} alt="" />
-        </div>  
+          <img className={styles.heroImage} src={profilePic2} alt="" />
+          <img className={styles.heroDecBottom} src={decBottom} alt="Main Illustration" />
+
+        </div>
     </section>
   );
 };
