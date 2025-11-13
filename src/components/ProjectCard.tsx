bc17614018ffb883d1089d09fc36c971eb330954
Hugo Lomba <98366477+hugolomba@users.noreply.github.com>
2025-11-13 11:37:00 +0000

    fixed link target project card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,8 +1,7 @@
-import Button from './Button'
-import styles from './ProjectCard.module.css'
+import Button from "./Button";
+import styles from "./ProjectCard.module.css";
 import { FaGithub } from "react-icons/fa";
 import { TbWorld } from "react-icons/tb";
-
 
 type Project = {
   title: string;
@@ -16,8 +15,8 @@ type Project = {
 };
 
 type ProjectCardProps = {
-    project: Project;
-}
+  project: Project;
+};
 
 let colorQueue: string[] = [];
 const backgroundColors: string[] = [
@@ -27,7 +26,7 @@ const backgroundColors: string[] = [
   "redBackground",
   "greenBackground",
   "petrolBlueBackground",
-  "softPurpleBackground"
+  "softPurpleBackground",
 ];
 
 function shuffle<T>(array: T[]): T[] {
@@ -41,49 +40,75 @@ function getNextBackgroundColor(): string {
   return colorQueue.shift() as string;
 }
 
-
-
-
 export default function ProjectCard({ project }: ProjectCardProps) {
+  const techColors: string[] = [
+    "jsColor",
+    "reactColor",
+    "nodeColor",
+    "mongoColor",
+    "expressColor",
+    "htmlColor",
+    "cssColor",
+    "muiColor",
+    "axiosColor",
+  ];
 
-    const techColors: string[] = [
-        "jsColor",
-        "reactColor",
-        "nodeColor",
-        "mongoColor",
-        "expressColor",
-        "htmlColor",
-        "cssColor",
-        "muiColor",
-        "axiosColor"
-    ];
+  function getTechColor(tech: string): string {
+    const index = project.technologies.indexOf(tech);
+    return index !== -1
+      ? techColors[index % techColors.length]
+      : "defaultColor";
+  }
 
-    function getTechColor(tech: string): string {
-        const index = project.technologies.indexOf(tech);
-        return index !== -1 ? techColors[index % techColors.length] : "defaultColor";
-    }
-
-    const backgroundColor = getNextBackgroundColor();
+  const backgroundColor = getNextBackgroundColor();
 
   return (
     <div className={`${styles.cardContainer} ${styles[backgroundColor]}`}>
-        <p className={styles.title}>{project.title}</p>
-        <div className={styles.imageContainer}>
-        <img src={project.images[0]} alt={project.title} className={styles.image} />
-        </div>
-           <div className={`${styles.technologies}`}>
-            {project.technologies.map((tech, index) => (
-                //
-                <span key={index} className={`${styles.technology} ${styles[getTechColor(tech)]}`}>{`#${tech} `}</span>
-            ))}
-        </div>
-        <p className={styles.description}>{project.shortDescription}</p>
-     
-        <div className={styles.links}>
-            <Button btnType='link' variant="cardButton" href={project.github} ><FaGithub /> GitHub</Button>
-            <Button btnType='link' variant="cardButton" href={project.link} ><TbWorld /> Demo</Button>
-        </div>
-        <Button btnType="link" variant="cardButton" href={`/project/${project.id}`} target="">See More</Button>
+      <p className={styles.title}>{project.title}</p>
+      <div className={styles.imageContainer}>
+        <img
+          src={project.images[0]}
+          alt={project.title}
+          className={styles.image}
+        />
+      </div>
+      <div className={`${styles.technologies}`}>
+        {project.technologies.map((tech, index) => (
+          //
+          <span
+            key={index}
+            className={`${styles.technology} ${styles[getTechColor(tech)]}`}
+          >{`#${tech} `}</span>
+        ))}
+      </div>
+      <p className={styles.description}>{project.shortDescription}</p>
+
+      <div className={styles.links}>
+        <Button
+          btnType="link"
+          variant="cardButton"
+          href={project.github}
+          target="_blank"
+        >
+          <FaGithub /> GitHub
+        </Button>
+        <Button
+          btnType="link"
+          variant="cardButton"
+          href={project.link}
+          target="_blank"
+        >
+          <TbWorld /> Demo
+        </Button>
+      </div>
+      <Button
+        btnType="link"
+        variant="cardButton"
+        href={`/project/${project.id}`}
+        target=""
+      >
+        See More
+      </Button>
     </div>
-  )
+  );
 }
